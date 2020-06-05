12d715a30edbd6bf46df72986ece4b83b2158df6
Angel G <a.genchev@mail.bg>
2020-06-06 02:47:37 +0300

    uploaded a working version
    do not use pHash1, it's broken

--- phash.php
+++ phash.php
@@ -1,50 +1,126 @@
 <?php
+ # based on links, code & comments from http://www.hackerfactor.com/blog/?/archives/529-Kind-of-Like-That.html 
  # requires php7
- # Status: non-working yet
+ # requires php GD library, install:  sudo apt install php-gd;sudo phpenmod gd;
+ # requires php-gmp, to install php-gmp in ubuntu 20: sudo apt install php-gmp;sudo phpenmod gmp;
+ # Status: dHash1 is working
  # TODO: make test-able methods public & a test program
-class NS_Hasher
+class NS_pHash
 {
 	private static $_dctConst = null;  
     private static function getDctConst():array
     {  # pre-cache DCT coefficients
        if(self::$_dctConst){ return self::$_dctConst;}  
-       $pi = pi();
+       
        self::$_dctConst = array();  
        for ($dctP=0; $dctP<8; $dctP++) 
        {  
            for ($p=0;$p<32;$p++) 
            {  
-               self::$_dctConst[$dctP][$p] = cos( ((2*$p + 1)/64) * $dctP * $pi );  
+               self::$_dctConst[$dctP][$p] = cos( ((2*$p + 1)/64) * $dctP * M_PI );  
            }  
        }  
       return self::$_dctConst;  
-   }  
+   }
    
-  private static function get_resampled_gray($fname, $wid, $hei)
+     
+    # returns 64-bit dHash
+    public static function dHash64($imagePath)
+    {  
+       $img = self::get_resampled_gray($imagePath, 9, 8);
+       if(!$img){ return NULL; }  
+  
+       $grays = array();  
+       for ($y=0; $y<8; $y++)
+       {  
+           for ($x=0; $x<9; $x++)
+           {  
+               $grays[$y][$x] = imagecolorat($img, $x, $y) & 0xFF;  
+           }  
+       }  
+       imagedestroy($img);
+       
+       # diffs:
+       $bits = array();
+       for ($y=0; $y<8; $y++)
+       {  
+           for ($x=0; $x<8; $x++)
+           {  
+               $bits[] = ($grays[$y][$x] < $grays[$y][$x+1]) ? '1' : '0';
+           }  
+       }  
+
+       return gmp_strval( gmp_init(implode($bits),2) ); # 64-bit unsigned number as string, ready for database insert query
+    }  
+   
+   
+   
+    # returns 16-bit dHash 
+    public static function dHash16($imagePath)
+    {  
+       $img = self::get_resampled_gray($imagePath, 6, 4); # 6x4 = 24
+       if(!$img){ return NULL; }  
+  
+       $grays = array();  
+       for ($y=0; $y<4; $y++)
+       {  
+           for ($x=0; $x<6; $x++)
+           {  
+               $grays[$y][$x] = imagecolorat($img, $x, $y) & 0xFF;  
+           }  
+       }  
+       imagedestroy($img);
+       
+       # calc diffs in x-direction
+       $bits = [];
+       for ($y=0; $y<4; $y++)
+       {  
+           for ($x=0; $x<5; $x++)
+           { 
+               $bits[] = ($grays[$y][$x] < $grays[$y][$x+1]) ? '1' : '0';
+           } 
+       }
+       # remove corner positions [0][0], [0][4], [3][0], [3][4]
+       unset($bits[0]); unset($bits[4]);
+       unset($bits[15]);unset($bits[19]); 
+       
+       return gmp_strval( gmp_init(implode($bits),2) ); # 64-bit unsigned number as string, ready for database insert query
+    }  
+    
+    
+   
+  private static function get_resampled_gray($ffname, $twid, $thei, $onlyJPG=TRUE)
   {
-  	$thumb = exif_thumbnail($fname, $twid, $thei, $onlyJPG=TRUE);
-  	if (!$thumb) 
-	{
-		$src = ($onlyJPG) ? imagecreatefromjpeg($fname) : imagecreatefromstring(file_get_contents($fname));
-		list($wid, $hei) = array(imagesx($src), imagesy($src));
-	} else 
-	{
-		$src = imagecreatefromstring($thumb);
-		unset($thumb);
+    try
+    {
+  		$thumb = exif_thumbnail($ffname, $swid, $shei, $stype);
+  		if (!$thumb) 
+		{
+			$src = ($onlyJPG) ? imagecreatefromjpeg($ffname) : imagecreatefromstring(file_get_contents($ffname));
+			list($swid, $shei) = array(imagesx($src), imagesy($src));
+		} else 
+		{
+			$src = imagecreatefromstring($thumb);
+			unset($thumb);
+		}
+		// resize to 32x32
+		$img = imagecreatetruecolor($twid, $thei);
+		imagecopyresampled($img, $src, 0, 0, 0, 0, $twid, $thei, $swid, $shei);
+		imagedestroy($src);
+		imagefilter($img, IMG_FILTER_GRAYSCALE);
 	}
-	// resize to 32x32
-	$img = imagecreatetruecolor($twid, $thei);
-	imagecopyresampled($img, $src, 0, 0, 0, 0, $twid, $thei, $wid, $hei);
-	imagedestroy($src);
-	imagefilter($img, IMG_FILTER_GRAYSCALE);
+	catch(Exception $e) 
+          		{ echo(__FILE__.'~72: Caught exception: '.$e->getMessage());
+          		  return NULL;
+          		}
 	return $img;
   }
   
-  
-  public static function hash1($fname):Hash
+  # perceptual hash1, slow DCT
+  public static function pHash1($ffname):string
     {
      // Resize the image.
-     $img = get_resampled_gray($fname,32,32);
+     $img = self::get_resampled_gray($ffname,32,32);
 
      $matrix = [];
      $row = [];
@@ -67,7 +143,7 @@ class NS_Hasher
             {
                 $col[$y] = $rows[$y][$x];
             }
-            $matrix[$x] = $this->calculateDCT1($col);
+            $matrix[$x] = self::calculateDCT1($col);
         }
 
         // Extract the top 8x8 pixels.
@@ -82,25 +158,25 @@ class NS_Hasher
 
         // Method === MEDIAN) 
         //{
-        //  $compare = $this->median($pixels);
+        $compare = self::median($pixels);
         //} else 
        // {
-            $compare = self::average($pixels);
+       # $compare = self::average($pixels);
        // }
-
+       
         // Calculate hash.
         $bits = [];
         foreach ($pixels as $pixel) 
         {
-            $bits[] = (int) ($pixel > $compare);
+            $bits[] = ($pixel > $compare) ? '1' : '0';
         }
 
-        return Hash::fromBits($bits);
+        // have array of chars '0' or '1'
+        return gmp_strval( gmp_init(implode($bits),2) ); # 64-bit unsigned number as string, ready for database insert query
     }
 
-    //
-    // * Perform a 1 dimension Discrete Cosine Transformation.
-    // 
+    
+    # Perform a 1 dimension Discrete Cosine Transformation.
     protected static function calculateDCT1(array $matrix): array
     {
         $transformed = [];
@@ -124,11 +200,11 @@ class NS_Hasher
         return $transformed;
     }
 
-    /**
-     * Get the median of the pixel values.
-     TODO: Check this !! looks fake
-     */
-    protected function median(array $pixels): float
+    
+    # Get the median of the values.
+    # The median compared to the mean (average) is that it is reflects the "typical value" of the set 
+    # based on the count of all values
+    protected static function median(array $pixels): float
     {
         sort($pixels, SORT_NUMERIC);
 
@@ -140,9 +216,7 @@ class NS_Hasher
         return $pixels[(int) floor(count($pixels) / 2)];
     }
 
-    /**
-     * Get the average of the pixel values.
-     */
+    # Get the average of the values without the first one.
     protected static function average(array $pixels): float
     {
         // Calculate the average value from top 8x8 pixels, except for the first one.
@@ -151,12 +225,92 @@ class NS_Hasher
     }
     
     
+    # Average Hash
+    public static function aHash($ffname)
+    {  
+       $img = self::get_resampled_gray($ffname,8,8);
+       if(!$img){ return null; }  
+  
+       $graySum = 0;  
+       $grays = array();  
+       for ($y=0; $y<8; $y++)
+       {  
+           for ($x=0; $x<8; $x++)
+           {  
+               $gray = imagecolorat($img, $x, $y) & 0xFF;
+               $grays[] = $gray;  
+               $graySum +=  $gray;  
+           }  
+       }  
+       imagedestroy($img);  
+  
+       $average = $graySum/64;  
+  
+       foreach ($grays as $i => $gray)
+       {
+         $grays[$i] = ($gray>=$average) ? '1' : '0';  
+       }
+  
+       return gmp_strval( gmp_init(implode($grays),2) ); # 64-bit unsigned number as string, ready for database insert query 
+     }  
+     
+     
+     
+     public static function pHash($ffname)
+     {  
+       $img = self::get_resampled_gray($ffname,32,32);
+       if(!$img){ return null; }  
+       
+       $grays = array();  
+       for ($y=0; $y<32; $y++)
+       {  
+          for ($x=0; $x<32; $x++)
+          {  
+               $grays[$y][$x] = imagecolorat($img, $x, $y) & 0xFF;  
+          }  
+       }  
+       imagedestroy($img);  
+  
+  
+       // DCT 8*8  
+       $dctConst = self::getDctConst();
+       $dctSum = 0;  
+   
+       $dcts = array();  
+       for ($dctY=0; $dctY<8; $dctY++) 
+       {  
+          for ($dctX=0; $dctX<8; $dctX++) 
+           {  
+            $sum = 1;  
+            for ($y=0;$y<32;$y++) 
+            {  
+               for ($x=0;$x<32;$x++) 
+                 {  
+                   $sum += $dctConst[$dctY][$y] * $dctConst[$dctX][$x] * $grays[$y][$x];  
+                 }  
+            }  
+  
+            // apply coefficients
+            $sum = $sum * 0.25;
+            if ($dctY == 0 || $dctX == 0) 
+            { # exclude term[0][0]
+              $sum = $sum * M_SQRT1_2; # M_SQRT1_2 == 1/sqrt(2);
+            }
+  
+            $dcts[] = $sum;
+            $dctSum +=  $sum;  
+           }  
+       }
+  
+      
+      $average = $dctSum/64;  
+        
+      foreach ($dcts as $i => $dct)
+      {  
+           $dcts[$i] = ($dct>=$average) ? '1' : '0';  
+      }  
+  
+     return gmp_strval( gmp_init(implode($dcts),2) ); # 64-bit unsigned number as string, ready for database insert query
+   }  
     
-    
-    }
-    
-    
-    
-    
-    
-    
+    } # end class
